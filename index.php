66fd6740e2e580fe57b41dffc4fcaff09160c961
MARCELA LUIZA RAMOS REIS <marcela.6966@aluno.pr.senac.br>
2023-03-06 08:45:09 -0300

    salvar

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+
+$nome ="Marcela";
+$idade="15";
+$endereco="avenida espirito santo";
+$cidade="ivaiporã";
+$cep="86.870-000";
+$primeirosemestre=8;
+$segundosemestre=10;
+$terceirosemestre=9;
+$media=($primeirosemestre+$segundosemestre+$terceirosemestre)/3;
+
+echo "o seu nome e " . $nome;
+echo("</br>");
+echo "sua idada e " . $idade;
+echo("</br>");
+echo "seu endereco é " . $endereco;
+echo("</br>");
+echo "sua cidade é" . $cidade;
+echo("</br>");
+echo "seu cep é " . $cep;
+echo("</br>");
+echo "seu primeiro semestre é " . $primeirosemestre;
+echo("</br>");
+echo "seu segundo semestre é " . $segundosemestre;
+echo("</br>");
+echo "seu terceiro semestre é " . $terceirosemestre;
+echo("</br>");
+echo "sua media é " .$media; 
+
+
+?>
